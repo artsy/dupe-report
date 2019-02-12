953dbb901372c3f28c30aafded71cc1802f35912
Justin Bennett <zephraph@gmail.com>
2019-02-12 18:05:39 -0500

    Initial commit

---
+++ src/duplicates-formatter.ts
@@ -0,0 +1,72 @@
+import zip from "lodash.zip";
+
+const chunkTitle = /^##\s.+\.js$/gm;
+const pkgHeader = /^[a-z-_@/.]+\s\(Found.+\)/gm;
+
+const notEmpty = (str: string) => str.length > 0;
+
+const splitAtLine = (line: number, string: string) => {
+  const lines = string.split("\n");
+  return [lines.slice(0, line).join("\n"), lines.slice(line).join("\n")];
+};
+
+const formatBody = bodyData =>
+  bodyData
+    .map(
+      ([chunk, depGroups]) =>
+        chunk +
+        "\n" +
+        depGroups
+          .map(depSet =>
+            [
+              "<details>",
+              `<summary>${depSet[0]}</summary>`,
+              "<pre>",
+              depSet[1]
+                .replace(/[\n]+/gm, "\n")
+                .replace(/^\s{2}/gm, "")
+                .trim(),
+              "</pre>",
+              "</details>"
+            ].join("\n")
+          )
+          .join("")
+    )
+    .join("\n\n");
+
+export const formatReport = (report: string) => {
+  let [header, bodyWithFooter] = splitAtLine(6, report);
+  const [body] = splitAtLine(-6, bodyWithFooter);
+
+  header = header.replace("* Duplicates", "**Duplicates**");
+  header = header.replace("* Packages", "**Packages**");
+  header = "## " + header;
+
+  const footer =
+    "\n<br/>\n\n" +
+    "[Fixing bundle duplicates](https://github.com/FormidableLabs/inspectpack/#fixing-bundle-duplicates)\n" +
+    "[An introductory guide](https://github.com/FormidableLabs/inspectpack/#diagnosing-duplicates)\n";
+
+  const bodyLines = body.split("\n");
+  const chunks = bodyLines
+    .filter(line => line.match(chunkTitle))
+    .map(chunk => "#" + chunk);
+
+  const chunkDependencies = body
+    .split(chunkTitle)
+    .filter(notEmpty)
+    .map(deps => {
+      const depLines = deps.split("\n").filter(notEmpty);
+      return zip(
+        depLines.filter(line => line.match(pkgHeader)),
+        deps
+          .split(pkgHeader)
+          .filter(notEmpty)
+          .slice(1)
+      );
+    });
+
+  const bodyData = zip(chunks, chunkDependencies);
+
+  return header + formatBody(bodyData) + footer;
+};
