11208e35a4911656ac83fa556b2cefc029b4d3be
Justin Bennett <zephraph@gmail.com>
2019-02-12 23:04:05 -0500

    Fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
 
 class ArtsyDupeReport extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    pr: flags.string({
+      description: "The PR this report is associated with",
+      required: true
+    }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" })
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(ArtsyDupeReport)
+    const { args, flags } = this.parse(ArtsyDupeReport);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
+    this.log(`hello ${name} from ./src/index.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
 
-export = ArtsyDupeReport
+export = ArtsyDupeReport;
